fb6447608089d93fcfce93851ff08122fea4a65d
Daniel Locke <97636427+danielocke@users.noreply.github.com>
2023-01-12 13:30:43 -0500

    Add files via upload

--- arScraper.py
+++ arScraper.py
@@ -1,17 +1,27 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as Soup
 import time
+import jellyfish
 
 #Number of recipes stored on each page of results by allrecipes website.
 RESULT_OFFSET_SIZE = 24
+
+#Allowed Jaro similarity between two strings for them to be considered the same ingredient/unit.
+ING_SIM_ALLOWANCE = 0.25
+UNIT_SIM_ALLOWANCE = 0.35
+
 #HTML classes/id tags used by allRecipes
 CLASS_RECIPE_CARD   = "comp mntl-card-list-items mntl-document-card mntl-card card card--no-image"
 CLASS_RECIPE_RATING = "recipe-card-meta__rating-count-number"
 ID_END_OF_RESULTS   = "search-results__no-results_1-0"
 CLASS_INGREDIENT    = "mntl-structured-ingredients__list-item"
+ID_RECIPE_RATING    = "mntl-recipe-review-bar__rating_2-0"
 TAG_INGR_QUANTITY   = "data-ingredient-quantity"
 TAG_INGR_UNIT       = "data-ingredient-unit"
 TAG_INGR_NAME       = "data-ingredient-name"
+
+#Dictionary to store ingredients and quantities  
+ingredients = {}
 
 def search(query):
 
@@ -91,8 +101,33 @@ def extractIngredients(recipe_s):
     
     return ingredients
 
+'''
 def extractSteps(recipe_s):
-    return False 
+   return False 
+'''
+
+def getRating(recipe_s):
+    return float(recipe_s.find("div",{"id":ID_RECIPE_RATING}).string)
+
+def catagorizeIngredients(ingList, rating):
+
+    for curIng in ingList:
+        for ing in ingredients:
+            if jellyfish.jaro_similarity(curIng[1].lower(),ing.lower()) > ING_SIM_ALLOWANCE:
+                ingredients[ing] += curIng[0]*rating
+                return 
+    ingredients[curIng[1]] = curIng[0]*rating  
+
+def convertIngQuantity(ingList):
+    units = {"cup":250,"c":250,
+             "gram":1,"g":1,
+             "millilitre":1,"ml":1,
+             "milligram":1/1000,"mg":1/1000,
+             "litre":1000, "l":1000,
+             "tablespoon":15,"tbsp":15,
+             "teaspoon":5,"tsp":5}
+    #for ing in ingList:
+
 
 def strToNum(string):
     fracDict = {"½":1/2,"⅓":1/3,"⅔":2/3,"¼":1/4,"¾":3/4,"⅛":1/8,"⅜":3/8,"⅝":5/8,"⅞":7/8}
@@ -119,9 +154,8 @@ start = time.time()
 recipeURLs = search("sushi")
 recipes = getRecipes(recipeURLs)
 
-
 for recipe in recipes:
-    print(extractIngredients(recipe))
+    print(str(getRating(recipe)) + " - " + str(extractIngredients(recipe)))
 
 end = time.time()
 print("Time: " + str(end-start))
